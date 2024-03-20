36b0980e87c7563d56c8c314424603436f6a3dbe
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-21 01:41:54 +0500

    commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var mygoodday = "These are the list i want to do in whole day to say it as a good day\n1-First of all i want to star my day by performing FAJAR salah\n2-After salah i want to do some exerercise to stay healthy\n3-I will clean my room by my own";
+console.log(mygoodday);
---
+++ index.ts
@@ -0,0 +1,6 @@
+let mygoodday = `These are the list i want to do in whole day to say it as a good day
+1-First of all i want to star my day by performing FAJAR salah
+2-After salah i want to do some exerercise to stay healthy
+3-I will clean my room by my own`
+
+console.log(mygoodday);
